facceea468f5295f57b4a8b5aa67aa80fecfb49e
Ron Hauder <ronhauder@gmail.com>
2016-11-02 18:58:37 -0500

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,44 +19,40 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force: true
 }).then(function() {
 	console.log('Everything is synced');
 
-	Todo.findById(3).then(function(todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('todo not found');
-		}
-	})
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				completed: false
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
+	});
 
-	// Todo.create({
-	// 	description: 'Take out trash',
-	// 	completed: false
-	// }).then(function(todo) {
+	// User.create({
+	// 	email: 'ron@example.com'
+	// }).then(function () {
 	// 	return Todo.create({
-	// 		description: 'Clean office'
-	// 	})
-	// }).then(function() {
-	// 	//return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%Office%'
-	// 			}
-	// 		}
+	// 		description: 'Clean yard'
 	// 	});
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function(todo) {
-	// 			console.log(todo.toJSON());
-	// 		});
-	// 	} else {
-	// 		console.lob('no todo found');
-	// 	}
-	// }).catch(function(e) {
-	// 	console.log(e);
-	// })
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user){
+	// 		user.addTodo(todo);
+	// 	});
+	// });
+
 });
--- server.js
+++ server.js
@@ -59,7 +59,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
-		res.json(todo.toJSON());
+		req.user.addTodo(todo).then(function() {
+			return todo.reload();
+		}).then(function(todo) {
+			res.json(todo.toJSON());
+		});
 	}, function(e) {
 		res.status(400).json(e);
 	});
@@ -93,23 +97,23 @@ app.put('/todos/:id', function(req, res) {
 
 	if (body.hasOwnProperty('completed')) {
 		attributes.completed = body.completed;
-	} 
+	}
 
 	if (body.hasOwnProperty('description')) {
 		attributes.description = body.description;
-	} 
+	}
 
-	db.todo.findById(todoID).then(function (todo) {
+	db.todo.findById(todoID).then(function(todo) {
 		if (todo) {
-			todo.update(attributes).then(function (todo) {
+			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
-			}, function (e) {
+			}, function(e) {
 				res.status(400).json(e);
 			});
 		} else {
 			res.status(404).send();
 		}
-	}, function () {
+	}, function() {
 		res.status(500).send();
 	});
 });
@@ -126,8 +130,8 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
-	
-	db.user.authenticate(body).then(function (user) {
+
+	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
 
 		if (token) {
@@ -135,13 +139,15 @@ app.post('/users/login', function(req, res) {
 		} else {
 			res.status(401).send();
 		}
-		
-	}, function () {
+
+	}, function() {
 		res.status(401).send();
 	});
 });
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
